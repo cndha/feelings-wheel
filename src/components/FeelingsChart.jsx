0843df05ced58ed8ceafbd27d0db6ff47873b148
Cindy Ha <cindyhaa@gmail.com>
2022-06-07 11:20:49 -0700

    web app foundations

---
+++ src/components/FeelingsChart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as d3 from "d3";
+import data from "../data.json";
+
+const FeelingsChart = () => {
+    return (
+        <>
+            {data.name}
+        </>
+    )
+}
+
+export default FeelingsChart;
